6297a664c8ac57e0826e053f65f5c0a8b0f41a78
Lucas <lucaslonguinhos@gmail.com>
2020-12-17 23:34:20 -0300

    GitKraken test 3

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,7 +10,7 @@ const theme = createMuiTheme({
       paper: colors.common.white,
     },
     primary: {
-      main: '#f44336',
+      main: '#f44337',
     },
     secondary: {
       main: '#3EA6FF',
