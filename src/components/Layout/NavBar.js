e0d0983a3644d6d2300c5f9d6038c7c2203a2450
Lucas <lucaslonguinhos@gmail.com>
2020-12-28 14:19:43 -0300

    Test Deploy Vercel

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -137,7 +137,7 @@ function NavBar() {
         {!session ? (
           <Box mx={4} my={2}>
             <Typography variant="body2">
-              Faça login para curtur vídeos, comentar e se inscrever.
+              (ALTERADO) Faça login para curtur vídeos, comentar e se inscrever.
             </Typography>
             <Box mt={2}>
               <Button
